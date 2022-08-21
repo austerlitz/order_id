44b5b13b881967988769bad5b2e8ff0cbe3eef59
Max Buslaev <max@buslaev.net>
2022-08-21 23:57:39 +0300

    very basic specs

--- lib/OrderId.rb
+++ lib/OrderId.rb
@@ -1,6 +1,26 @@
 require "OrderId/version"
-
+require 'bigdecimal'
 module OrderId
-  class Error < StandardError; end
-  # Your code goes here...
+  DECIMAL_LENGTH = 20
+  BASE           = 36
+  SEPARATOR      = '-'
+  GROUP_LENGTH   = 5
+  class FormatError < StandardError; end
+
+  def self.generate(length: DECIMAL_LENGTH, base: BASE, separator: SEPARATOR, group_length: GROUP_LENGTH)
+    if separator =~ (/[\w\d]/)
+      raise FormatError, "Characters not allowed as separator: `#{separator}`"
+    end
+    t  = Time.now.to_f
+    ts = "%.#{length}f" % t
+    ts.delete!('.')
+    final = ts.to_i.to_s(base).upcase
+    final.scan(/.{1,#{group_length}}/).join(separator)
+  end
+
+  def self.get_time(id, length: DECIMAL_LENGTH, base: BASE, separator: SEPARATOR)
+    id.delete!(separator)
+    ts = id.to_i(base) / BigDecimal("1e+#{length}")
+    Time.at(ts)
+  end
 end
--- spec/OrderId_spec.rb
+++ spec/OrderId_spec.rb
@@ -3,7 +3,23 @@ RSpec.describe OrderId do
     expect(OrderId::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it { should respond_to(:generate) }
+  it { should respond_to(:get_time) }
+
+  describe '.generate' do
+    context 'with defaults' do
+      subject { described_class.generate }
+      it 'generates a string 22 chars long' do
+        expect(subject.length).to eq 22
+      end
+    end
+  end
+  describe '.get_time' do
+    context 'with defaults' do
+      let(:input) {described_class.generate}
+      it 'restores a timestamp from an input string' do
+        expect(described_class.get_time(input)).to be_a Time
+      end
+    end
   end
 end
