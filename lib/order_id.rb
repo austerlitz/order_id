428a91616e3075fcc07a347490add2f198d5cf5e
Max Buslaev <max@buslaev.net>
2023-07-31 11:12:50 +0300

    rename `length`->`decimal_places` to avoid ambiguity; set a closed list of allowed separator chars

--- lib/order_id.rb
+++ lib/order_id.rb
@@ -1,26 +1,27 @@
-require "order_id/version"
+require 'order_id/version'
 require 'bigdecimal'
 module OrderId
-  DECIMAL_LENGTH = 20
+  DECIMAL_PLACES = 20
   BASE           = 36
   SEPARATOR      = '-'
   GROUP_LENGTH   = 5
+  ALLOWED_SEPARATORS = ['-', '_', '|', ':', '@', '.', '/', '#', '!', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}'].freeze
+
   class FormatError < StandardError; end
 
-  def self.generate(length: DECIMAL_LENGTH, base: BASE, separator: SEPARATOR, group_length: GROUP_LENGTH)
-    if separator =~ (/[\w\d]/)
-      raise FormatError, "Characters not allowed as separator: `#{separator}`"
-    end
-    t  = Time.now.to_f
-    ts = "%.#{length}f" % t
-    ts.delete!('.')
+  def self.generate(decimal_places: DECIMAL_PLACES, base: BASE, separator: SEPARATOR, group_length: GROUP_LENGTH)
+    raise FormatError, "Characters not allowed as separator: '#{separator}'" unless ALLOWED_SEPARATORS.include?(separator)
+    raise FormatError, 'Length should be positive' unless decimal_places.positive?
+
+    t = Time.now.to_f
+    ts = format("%.#{decimal_places}f", t).delete('.')
     final = ts.to_i.to_s(base).upcase
     final.scan(/.{1,#{group_length}}/).join(separator)
   end
 
-  def self.get_time(id, length: DECIMAL_LENGTH, base: BASE, separator: SEPARATOR)
+  def self.get_time(id, decimal_places: DECIMAL_PLACES, base: BASE, separator: SEPARATOR)
     id.delete!(separator)
-    ts = id.to_i(base) / BigDecimal("1e+#{length}")
+    ts = id.to_i(base) / BigDecimal("1e+#{decimal_places}")
     Time.at(ts)
   end
 end
